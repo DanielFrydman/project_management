e9ab4ba508e4daf83fda764cd66adbafaee243ce
Daniel David Frydman <daniel843347@gmail.com>
2025-02-04 17:44:38 +0100

    ✨ Add demo user setup in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    Current.user = User.first_or_create!(
+      email: 'demo-user@example.com',
+      name: 'Demo User'
+    )
+  end
 end
