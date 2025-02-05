bb6ba39e853c72b352533d5d7fac66f49b071f20
Daniel David Frydman <daniel843347@gmail.com>
2025-02-05 06:48:46 +0100

    🚚  Adding new routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,13 @@ Rails.application.routes.draw do
     end
   end
 
-  root "projects#show"
+  # Authentication
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'registrations#new'
+  post 'signup', to: 'registrations#create'
+
+  root "projects#index"
 end
